dbcf481aed02b17a4dc9163240baec0d475ce81a
venkatakrishnam.aws <vk2860094@gmail.com>
2026-02-03 13:26:55 +0530

    This is my latest push

--- catalogue.sh
+++ catalogue.sh
@@ -34,62 +34,55 @@ validate_installation() {
   fi
 }
 
-# Function to install packages
-install_package() {
-  PACKAGE=$1
-  if ! rpm -q $PACKAGE &>/dev/null; then
-      echo -e "${yellow}$PACKAGE not installed. Installing...${nocolor}" | tee -a $log_file
-      dnf install -y $PACKAGE
-      validate_installation $? "$PACKAGE installation"
-  else
-      echo -e "${green}$PACKAGE is already installed.${nocolor}" | tee -a $log_file
-  fi
-}
-
-dnf module disable nodejs -y  | tee -a $log_file
+# NodeJS setup
+dnf module disable nodejs -y | tee -a $log_file
 validate_installation $? "Disabling nodejs module"
 
-dnf module enable nodejs:20 -y  | tee -a $log_file
+dnf module enable nodejs:20 -y | tee -a $log_file
 validate_installation $? "Enabling nodejs 20 module"
 
-dnf install nodejs -y  | tee -a $log_file
+dnf install nodejs -y | tee -a $log_file
 validate_installation $? "Installing nodejs"
 
+# App user
 useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop | tee -a $log_file
 validate_installation $? "Adding roboshop user" 
 
-mkdir /app | tee -a $log_file
-validate $? "Creating /app directory"
+mkdir -p /app | tee -a $log_file
+validate_installation $? "Creating /app directory"
 
+# Catalogue service setup
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
 validate_installation $? "Downloading catalogue code"
 
 cd /app
 unzip /tmp/catalogue.zip
-validate_installation $? "unzipping the files" 
+validate_installation $? "Unzipping catalogue files" 
 
 npm install 
 validate_installation $? "Installing nodejs dependencies"
 
-cp $scriptdir/catalogue.service /etc/systemd/system/catalogue.service
+cp $scriptdir/catalogue/catalogue.service /etc/systemd/system/catalogue.service
 validate_installation $? "Copying catalogue systemd file"
+
 systemctl daemon-reload
 validate_installation $? "Reloading systemd daemon"
+
 systemctl enable catalogue
 validate_installation $? "Enabling catalogue service"
+
 systemctl start catalogue
 validate_installation $? "Starting catalogue service"
-echo -e "${green}script ended at: $(date)${nocolor}" | tee -a $log_file
 
-cp $scriptdir/mongo.repo /etc/yum.repos.d/mongo.repo
-validate_installation $? "Copying MongoDB repo file"
+# MongoDB repo setup
+curl -o /etc/yum.repos.d/mongo.repo https://raw.githubusercontent.com/daws-84s/roboshop-documentation/main/mongo.repo
+validate_installation $? "Downloading MongoDB repo file"
 
 dnf install mongodb-mongosh -y
 validate_installation $? "Installing MongoDB Shell"
-echo -e "${green}MongoDB Shell installed successfully${nocolor}" | tee -a $log_file
-mongosh --host 3.90.232.170 </catalogue/mongo.js
+
+mongosh --host 3.90.232.170 </app/schema/mongo.js
 validate_installation $? "Loading catalogue schema to MongoDB"
+
 echo -e "${green}Catalogue setup completed successfully${nocolor}" | tee -a $log_file
-
-
-
+echo -e "${green}script ended at: $(date)${nocolor}" | tee -a $log_file
